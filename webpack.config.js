d10f2bff7966aaf9784de638a18e6ec6ce6b9859
inshuya <inshuyamuthukumar@gmail.com>
2021-03-02 23:46:40 -0600

    Changed url

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: 'http://3.141.190.187:4000'
         })
     }
 }
